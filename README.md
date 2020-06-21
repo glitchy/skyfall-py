9aa2e88267bbb98f02f31fb1a0b6a596cb27f4c3
System Administrator <root@Carries-MacBook-Pro.local>
2020-06-21 13:52:25 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+This program is a recreation of a computer program found in a scene from the movie SKYFALL.
+
+
+DIRECTIONS:
+
+(1) download repo </br>
+(2) run `python think_on_your_sins.py` from command line </br>
---
+++ skyfall.py
@@ -0,0 +1,57 @@
+import curses 
+import time 
+import sys
+import random
+import os
+
+# gets the terminals screen size by lines and columns
+os.get_terminal_size()
+
+# clears the terminal screen
+stdscr = curses.initscr()
+curses.noecho()
+stdscr.keypad(True)
+
+quote = "THINK ON YOUR SINS".center(os.get_terminal_size().columns)
+words = list(quote)
+
+time.sleep(5)
+
+# makes cursor invisible
+curses.curs_set(False)
+
+# moves cursor halfway down window
+print(int((os.get_terminal_size().lines / 2) - 2) * '\n')
+
+# iterates through every value in list
+for letter in words:
+    time.sleep(random.uniform(.03, .05))
+    # prints on same line
+    print(letter, end='')
+    sys.stdout.flush()
+
+time.sleep(3)
+
+# screen refresh
+stdscr.refresh()
+
+# makes cursor invisible again
+curses.curs_set(True)
+
+# undo comments below for additional ending
+# paragraph = "THINK ON YOUR SINS " * 150
+# text = list(paragraph)
+time.sleep(4)
+
+# for letter in text:
+#    time.sleep(.005)
+#    # prints on same line
+#    print(letter, end='')
+#    sys.stdout.flush()
+
+
+# properly ends program
+curses.nocbreak()
+stdscr.keypad(False)
+curses.echo()
+curses.endwin()
